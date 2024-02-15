1a9775d6f0a6671399661561eb16f8639c1799e2
nnmchang <121856985+nnmchang@users.noreply.github.com>
2024-02-15 04:44:44 +0000

    fix: デザイン修正に合わせて調整

--- src/lib/components/prose/img.svelte
+++ src/lib/components/prose/img.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
-  const setsets = import.meta.glob<string>(['/src/static/**/*.{jpg,jpeg,png,webp,avif}', '!/src/static/assets'], {
+  const setsets_avif = import.meta.glob<string>(['/src/static/**/*.{jpg,jpeg,png,webp,avif}', '!/src/static/assets'], {
     query: {
       format: 'avif',
       quality: '80',
-      w: '736',
+      w: '1024',
+      as: 'srcset'
+    },
+    import: 'default',
+    eager: true
+  })
+
+  const setsets_jpg = import.meta.glob<string>(['/src/static/**/*.{jpg,jpeg,png,webp,avif}', '!/src/static/assets'], {
+    query: {
+      format: 'jpg',
+      quality: '80',
+      w: '1024',
       as: 'srcset'
     },
     import: 'default',
@@ -16,14 +27,16 @@
   export let alt: string = src
   export let loading: 'eager' | 'lazy' = 'lazy'
   export let decoding: 'async' | 'sync' | 'auto' = 'async'
-  let srcset: string | undefined = setsets[`/src/static${src}`]
+  let srcset_avif: string | undefined = setsets_avif[`/src/static${src}`]
+  let srcset_jpg: string | undefined = setsets_jpg[`/src/static${src}`]
 </script>
 
-{#if srcset}
-  <picture>
-    <source {srcset} type="image/avif" />
-    <img {src} {alt} class={className ?? 'rounded-lg my-2'} {loading} {decoding} />
-  </picture>
-{:else}
+<picture>
+  {#if srcset_avif}
+    <source srcset={srcset_avif} type="image/avif" />
+  {/if}
+  {#if srcset_jpg}
+    <source srcset={srcset_jpg} type="image/jpeg" />
+  {/if}
   <img {src} {alt} class={className ?? 'rounded-lg my-2'} {loading} {decoding} />
-{/if}
+</picture>
